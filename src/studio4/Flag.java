779ac13871bb498916932c1ee780a2808305c7f8
battertonjack <batterton.jack@wustl.edu>
2022-10-13 13:53:18 -0500

    greatest flag of all time

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,11 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(0, 0, 255);	
+		StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.2);
+		StdDraw.setPenColor(0, 255, 255);
+		StdDraw.rectangle(0.5, 0.5, 0.2, 0.2);
+		StdDraw.setPenColor(225, 225, 0);	
+		StdDraw.rectangle(0.5, 0.5, 0.1, 0.1);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,62 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeName = in.next();
+		
+		int red = in.nextInt();
+		int green = in.nextInt();
+		int blue = in.nextInt();
+		
+		boolean isFilled = in.nextBoolean();
+		
+		StdDraw.setPenColor(red, green, blue);
+		
+		//rectangle
+		if (shapeName.equals("rectangle")) {
+			double x = in.nextDouble();
+			double y = in.nextDouble();
+			double halfWidth = in.nextDouble();
+			double halfHeight = in.nextDouble();
+			if (isFilled == true) {
+				StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+			}
+			else {
+				StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+
+		}
+		//triangle
+		if (shapeName.equals("triangle")) {
+			double one = in.nextDouble();
+			double two = in.nextDouble();
+			double three = in.nextDouble();
+			double four = in.nextDouble();
+			double five = in.nextDouble();
+			double six = in.nextDouble();
+			
+			double [] array1 = {one, three, five};
+			double [] array2 = {two, four, six};
+			
+			if (isFilled == true) {
+				StdDraw.filledPolygon(array1, array2);
+			}
+			else {
+				StdDraw.polygon(array1, array2);
+			}
+		}
+		//Ellipse
+		if (shapeName.equals("ellipse")) {
+			double one = in.nextDouble();
+			double two = in.nextDouble();
+			double three = in.nextDouble();
+			double four = in.nextDouble();
+						
+			if (isFilled == true) {
+				StdDraw.filledEllipse(one, two, three, four);
+			}
+			if (isFilled == false) {
+				StdDraw.ellipse(one, two, three, four);
+			}
+		}
 	}
 }
